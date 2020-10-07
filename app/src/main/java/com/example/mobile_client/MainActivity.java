2049d4d4ef0ce0bacf1c273a64430bb1e8fbe42f
hewagekasunthilina <kasunthilina1000@gmail.com>
2020-10-07 19:54:46 +0530

    Create login and main UI

--- app/src/main/java/com/example/mobile_client/MainActivity.java
+++ app/src/main/java/com/example/mobile_client/MainActivity.java
@@ -1,14 +1,38 @@
 package com.example.mobile_client;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    private CardView sitem;
+    private CardView sup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        sitem = (CardView)findViewById(R.id.sitemanager);
+        sitem.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent1 = new Intent(MainActivity.this, Login.class);
+                startActivity(intent1);
+            }
+        });
+
+        sup = (CardView)findViewById(R.id.supplier);
+        sup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent2 = new Intent(MainActivity.this, Supplier_Login.class);
+                startActivity(intent2);
+            }
+        });
     }
 }
