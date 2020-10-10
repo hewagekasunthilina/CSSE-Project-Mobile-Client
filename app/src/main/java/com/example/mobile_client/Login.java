f59441f8ab4ab07ce437c4dc92ebc19f82a5c057
hewagekasunthilina <kasunthilina1000@gmail.com>
2020-10-10 23:33:07 +0530

    API and login created successfully

--- app/src/main/java/com/example/mobile_client/Login.java
+++ app/src/main/java/com/example/mobile_client/Login.java
@@ -2,13 +2,74 @@ package com.example.mobile_client;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.google.android.material.textfield.TextInputEditText;
+
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class Login extends AppCompatActivity {
+
+    private TextInputEditText textusername, textpassword;
+    private Button btnlogin;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        textusername = (TextInputEditText) findViewById(R.id.username);
+        textpassword = (TextInputEditText) findViewById(R.id.password);
+        btnlogin = (Button) findViewById(R.id.smLogin);
+
+        btnlogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                login();
+            }
+        });
+
+    }
+
+    public void login(){
+        StringRequest request = new StringRequest(Request.Method.POST, "http://mail.dimodigital.lk/suplogin.php", new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                if (response.contains("1")){
+                    startActivity(new Intent(getApplicationContext(),Supplier_Login.class));
+                }else {
+                    Toast.makeText(getApplicationContext(),"Wrong Username or Password",Toast.LENGTH_SHORT).show();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+            }
+        }) {
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError{
+                Map<String, String> params = new HashMap<>();
+                params.put("username", textusername.getText().toString());
+                params.put("password", textpassword.getText().toString());
+                return params;
+            }
+        };
+        Volley.newRequestQueue(this).add(request);
     }
 }
