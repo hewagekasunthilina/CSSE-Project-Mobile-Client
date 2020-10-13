5bc2364f8a892371074a631a101d6d19df72c1ba
hewagekasunthilina <kasunthilina1000@gmail.com>
2020-10-14 04:08:36 +0530

    Dashboards updated

--- app/src/main/java/com/example/mobile_client/Site_Manager_Dashboard.java
+++ app/src/main/java/com/example/mobile_client/Site_Manager_Dashboard.java
@@ -1,6 +1,7 @@
 package com.example.mobile_client;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -9,15 +10,15 @@ import android.widget.Button;
 
 public class Site_Manager_Dashboard extends AppCompatActivity {
 
-    private Button btn;
+    private CardView create, view;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_site__manager__dashboard);
 
-        btn = (Button)findViewById(R.id.button);
-        btn.setOnClickListener(new View.OnClickListener() {
+        view = (CardView) findViewById(R.id.ViewPo);
+        view.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(Site_Manager_Dashboard.this,View_PO.class);
