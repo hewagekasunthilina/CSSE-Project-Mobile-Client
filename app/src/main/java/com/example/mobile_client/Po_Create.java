56b3cf0d73fe47f2e2a19fbecbf5afb8d3500111
gewagekasunthilina <kasunthilina1000@gmail.com>
2020-10-17 19:55:16 +0530

    PO create validation added

--- app/src/main/java/com/example/mobile_client/Po_Create.java
+++ app/src/main/java/com/example/mobile_client/Po_Create.java
@@ -39,7 +39,7 @@ public class Po_Create extends AppCompatActivity {
     ArrayAdapter<String> materialAdapter;
     RequestQueue requestQueue;
     private Button po;
-    private TextInputEditText RefNo,Material,Description, Price, Quantity, Site, DelDate, Status, CheckedBy;
+    private TextInputEditText RefNo, Material, Description, Price, Quantity, Site, DelDate, Status, CheckedBy;
 
 
     @Override
@@ -47,61 +47,86 @@ public class Po_Create extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_po__create);
 
-        RefNo = (TextInputEditText)findViewById(R.id.RefNo);
-        Material = (TextInputEditText)findViewById(R.id.Material);
-        Description = (TextInputEditText)findViewById(R.id.Description);
-        spinner1 = (Spinner)findViewById(R.id.spinner);
-        Price = (TextInputEditText)findViewById(R.id.Price);
-        Quantity = (TextInputEditText)findViewById(R.id.Quantity);
-        Site = (TextInputEditText)findViewById(R.id.Site);
-        DelDate = (TextInputEditText)findViewById(R.id.DelDate);
-        Status = (TextInputEditText)findViewById(R.id.Status);
-        CheckedBy = (TextInputEditText)findViewById(R.id.CheckedBy);
+        RefNo = (TextInputEditText) findViewById(R.id.RefNo);
+        Material = (TextInputEditText) findViewById(R.id.Material);
+        Description = (TextInputEditText) findViewById(R.id.Description);
+        spinner1 = (Spinner) findViewById(R.id.spinner);
+        Price = (TextInputEditText) findViewById(R.id.Price);
+        Quantity = (TextInputEditText) findViewById(R.id.Quantity);
+        Site = (TextInputEditText) findViewById(R.id.Site);
+        DelDate = (TextInputEditText) findViewById(R.id.DelDate);
+        Status = (TextInputEditText) findViewById(R.id.Status);
+        CheckedBy = (TextInputEditText) findViewById(R.id.CheckedBy);
 
-        po = (Button)findViewById(R.id.addPo);
+        po = (Button) findViewById(R.id.addPo);
         po.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                AsyncTask.execute(new Runnable() {
-                    @Override
-                    public void run() {
-                        OkHttpClient client = new OkHttpClient().newBuilder()
-                                .build();
-                        MediaType mediaType = MediaType.parse("text/plain");
-                        RequestBody body = new MultipartBody.Builder().setType(MultipartBody.FORM)
-                                .addFormDataPart("RefNo", RefNo.getText().toString())
-                                .addFormDataPart("Material", Material.getText().toString())
-                                .addFormDataPart("Description", Description.getText().toString())
-                                .addFormDataPart("Supplier", spinner1.getSelectedItem().toString())
-                                .addFormDataPart("Price", Price.getText().toString())
-                                .addFormDataPart("Quantity", Quantity.getText().toString())
-                                .addFormDataPart("Site", Site.getText().toString())
-                                .addFormDataPart("DelDate", DelDate.getText().toString())
-                                //.addFormDataPart("Status", Status.getText().toString())
-                                //.addFormDataPart("CheckedBy", CheckedBy.getText().toString())
-                                .build();
-                        okhttp3.Request request = new okhttp3.Request.Builder()
-                                .url("http://mail.dimodigital.lk/create_po.php")
-                                .method("POST", body)
-                                .build();
-                        try {
-                            okhttp3.Response response = client.newCall(request).execute();
-                        } catch (IOException e) {
-                            e.printStackTrace();
-                        }
-                        openViewPo();
-                    }
 
-                    private void openViewPo() {
-                        Intent intent = new Intent(Po_Create.this, View_PO.class);
-                        startActivity(intent);
-                    }
-                });
+                String NameInput = RefNo.getText().toString().trim();
+                String MaterialInput = Material.getText().toString().trim();
+                String DescInput = Description.getText().toString().trim();
+                String PriceInput = Price.getText().toString().trim();
+                String QtyInput = Quantity.getText().toString().trim();
+                String SiteInput = Site.getText().toString().trim();
+                String DateInput = DelDate.getText().toString().trim();
+
+                if (NameInput.isEmpty()) {
+                    RefNo.setError("Reference Number Can't be Empty");
+                } else if (MaterialInput.isEmpty()) {
+                    Material.setError("Material Can't be Empty");
+                } else if (DescInput.isEmpty()) {
+                    Description.setError("Description Can't be Empty");
+                } else if (PriceInput.isEmpty()) {
+                    Price.setError("Price Can't be Empty");
+                } else if (QtyInput.isEmpty()) {
+                    Quantity.setError("Quantity Can't be Empty");
+                } else if (SiteInput.isEmpty()) {
+                    Site.setError("Site Can't be Empty");
+                } else if (DateInput.isEmpty()) {
+                    DelDate.setError("Date Can't be Empty");
+                } else {
+                    AsyncTask.execute(new Runnable() {
+                        @Override
+                        public void run() {
+                            OkHttpClient client = new OkHttpClient().newBuilder()
+                                    .build();
+                            MediaType mediaType = MediaType.parse("text/plain");
+                            RequestBody body = new MultipartBody.Builder().setType(MultipartBody.FORM)
+                                    .addFormDataPart("RefNo", RefNo.getText().toString())
+                                    .addFormDataPart("Material", Material.getText().toString())
+                                    .addFormDataPart("Description", Description.getText().toString())
+                                    .addFormDataPart("Supplier", spinner1.getSelectedItem().toString())
+                                    .addFormDataPart("Price", Price.getText().toString())
+                                    .addFormDataPart("Quantity", Quantity.getText().toString())
+                                    .addFormDataPart("Site", Site.getText().toString())
+                                    .addFormDataPart("DelDate", DelDate.getText().toString())
+                                    //.addFormDataPart("Status", Status.getText().toString())
+                                    //.addFormDataPart("CheckedBy", CheckedBy.getText().toString())
+                                    .build();
+                            okhttp3.Request request = new okhttp3.Request.Builder()
+                                    .url("http://mail.dimodigital.lk/create_po.php")
+                                    .method("POST", body)
+                                    .build();
+                            try {
+                                okhttp3.Response response = client.newCall(request).execute();
+                            } catch (IOException e) {
+                                e.printStackTrace();
+                            }
+                            openViewPo();
+                        }
+
+                        private void openViewPo() {
+                            Intent intent = new Intent(Po_Create.this, View_PO.class);
+                            startActivity(intent);
+                        }
+                    });
+                }
             }
         });
 
         requestQueue = Volley.newRequestQueue(this);
-        spinner1 = (Spinner)findViewById(R.id.spinner);
+        spinner1 = (Spinner) findViewById(R.id.spinner);
         String url = "http://mail.dimodigital.lk/supplier_list.php";
 
         JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.POST, url, null, new Response.Listener<JSONObject>() {
@@ -126,10 +151,10 @@ public class Po_Create extends AppCompatActivity {
             public void onErrorResponse(VolleyError error) {
 
             }
-    });
+        });
 
         requestQueue.add(jsonObjectRequest);
 
 
-        }
+    }
 }
